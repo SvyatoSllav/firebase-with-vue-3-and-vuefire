939278a4f1450a826567c60604300676698161ba
polarius <tarasov.slavas2002@gmail.com>
2025-01-26 18:40:38 +0300

    Добавил firebase

--- src/main.js
+++ src/main.js
@@ -9,6 +9,8 @@ import '@mdi/font/css/materialdesignicons.css'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { VueFire } from 'vuefire'
+import { firebaseApp } from '@/fiebase'
 
 const vuetify = createVuetify({
   components,
@@ -19,5 +21,10 @@ const app = createApp(App)
 
 app.use(router)
 app.use(vuetify)
+app.use(VueFire, {
+  // imported above but could also just be created here
+  firebaseApp,
+  modules: [],
+})
 
 app.mount('#app')
